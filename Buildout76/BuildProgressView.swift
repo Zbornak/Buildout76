6c5f06322dcc0952418fd0e5746a34c4d6057b79
Mark <zbornack@protonmail.com>
2023-04-12 11:26:49 +0100

    Added build progress view

---
+++ Buildout76/BuildProgressView.swift
@@ -0,0 +1,20 @@
+//
+//  BuildProgressView.swift
+//  Buildout76
+//
+//  Created by Mark Strijdom on 12/04/2023.
+//
+
+import SwiftUI
+
+struct BuildProgressView: View {
+    var body: some View {
+        Text("Build in progress...")
+    }
+}
+
+struct BuildProgressView_Previews: PreviewProvider {
+    static var previews: some View {
+        BuildProgressView()
+    }
+}
