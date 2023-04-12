ae219edeb2a696f3361b8afd9940a4ee9d30410e
Mark <zbornack@protonmail.com>
2023-04-12 09:56:34 +0100

    Initial Commit

---
+++ Buildout76/Buildout76App.swift
@@ -0,0 +1,17 @@
+//
+//  Buildout76App.swift
+//  Buildout76
+//
+//  Created by Mark Strijdom on 12/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct Buildout76App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
