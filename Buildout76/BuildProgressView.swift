b7778bbb373280b986654dcc91b6caa7c5452a1a
Mark <zbornack@protonmail.com>
2023-04-12 18:27:57 +0100

    Still working on showing chosen perks under correct headings in buildprogress view

--- Buildout76/BuildProgressView.swift
+++ Buildout76/BuildProgressView.swift
@@ -8,31 +8,77 @@
 import SwiftUI
 
 struct BuildProgressView: View {
+    @EnvironmentObject var pickedPerks: PickedPerks
+    
     @State private var buildName = ""
+    
+    enum FilterType {
+        case strength, perception, endurance, charisma, intelligence, agility, luck
+    }
+    
+    let filter: FilterType
+    
+    var header: String {
+        switch filter {
+        case .strength:
+            return "strength perks"
+        case .perception:
+            return "perception perks"
+        case .endurance:
+            return "endurance perks"
+        case .charisma:
+            return "charisma perks"
+        case .intelligence:
+            return "intelligence perks"
+        case .agility:
+            return "agility perks"
+        case .luck:
+            return "luck perks"
+        }
+    }
+    
+    var filteredPickedPerks: [PickedPerk] {
+        switch filter {
+        case .strength:
+            return pickedPerks.pickedPerks.filter { $0.perk.specialCategory == "Strength" }
+        case .perception:
+            return pickedPerks.pickedPerks.filter { $0.perk.specialCategory == "Perception" }
+        case .endurance:
+            return pickedPerks.pickedPerks.filter { $0.perk.specialCategory == "Endurance" }
+        case .charisma:
+            return pickedPerks.pickedPerks.filter { $0.perk.specialCategory == "Charisma" }
+        case .intelligence:
+            return pickedPerks.pickedPerks.filter { $0.perk.specialCategory == "Intelligence" }
+        case .agility:
+            return pickedPerks.pickedPerks.filter { $0.perk.specialCategory == "Agility" }
+        case .luck:
+            return pickedPerks.pickedPerks.filter { $0.perk.specialCategory == "Luck" }
+        }
+    }
     
     var body: some View {
         Form {
-            Section {
-                TextField("Enter build name", text: $buildName)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .padding(.horizontal)
-            } header: {
-                Text("build name")
-            }
+            TextField("Enter build name", text: $buildName)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding(.horizontal)
             
-            FilteredProgressView(filter: .strength)
-            FilteredProgressView(filter: .perception)
-            FilteredProgressView(filter: .endurance)
-            FilteredProgressView(filter: .charisma)
-            FilteredProgressView(filter: .intelligence)
-            FilteredProgressView(filter: .agility)
-            FilteredProgressView(filter: .luck)
+            Section {
+                ForEach(filteredPickedPerks) { perk in
+                    HStack {
+                        Text(perk.perk.name)
+                        Text("\(perk.perkLevel)")
+                    }
+                }
+            } header: {
+                Text(header)
+            }
         }
     }
 }
 
 struct BuildProgressView_Previews: PreviewProvider {
     static var previews: some View {
-        BuildProgressView()
+        BuildProgressView(filter: .strength)
+            .environmentObject(PickedPerks())
     }
 }
--- Buildout76/ContentView.swift
+++ Buildout76/ContentView.swift
@@ -18,7 +18,7 @@ struct ContentView: View {
                     Label("Perks", systemImage: "person.crop.circle.fill.badge.plus")
                 }
             
-            BuildProgressView()
+            BuildProgressView(filter: .strength)
                 .tabItem {
                     Label("In Progress", systemImage: "gearshape.2")
                 }
