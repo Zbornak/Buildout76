e6c04d1cb60dd9d97a29a6dcc0ada91b2e4f618e
Mark <zbornack@protonmail.com>
2023-04-14 15:49:03 +0100

    Minor change

--- Buildout76/BuildProgressView.swift
+++ Buildout76/BuildProgressView.swift
@@ -102,7 +102,7 @@ struct BuildProgressView: View {
                     }
                 }
             }
-            .navigationTitle("Build in Progress")
+            .navigationTitle("☢️ Build in Progress")
             .alert("Build saved!", isPresented: $buildSaved) {
                 Button("OK", role: .cancel) { }
             }
