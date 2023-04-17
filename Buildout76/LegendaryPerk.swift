3449cf69f212e973fcb74c2c139a8afc2802afd0
Mark <zbornack@protonmail.com>
2023-04-17 16:20:24 +0100

    Added legendary perk struct

---
+++ Buildout76/LegendaryPerk.swift
@@ -0,0 +1,20 @@
+//
+//  LegendaryPerk.swift
+//  Buildout76
+//
+//  Created by Mark Strijdom on 17/04/2023.
+//
+
+import SwiftUI
+
+struct LegendaryPerk: Codable, Identifiable, Equatable {
+    var id = UUID()
+    let name: String
+    static let maxLevel = 4
+    let description1: String
+    let description2: String
+    let description3: String
+    let description4: String
+    
+    static let example = LegendaryPerk(name: "Sizzling Style", description1: "+50 Fire Damage Resist when wearing a matching set of armor.", description2: "+100 Fire Damage Resist when wearing a matching set of armor.", description3: "+150 Fire Damage Resist when wearing a matching set of armor.", description4: "+200 Fire Damage Resist when wearing a matching set of armor.")
+}
