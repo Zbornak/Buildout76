56172c6ee5b87ac0aa8e054ffd3fe327b4341020
Mark <zbornack@protonmail.com>
2023-04-17 09:29:13 +0100

    Working on filtering build detail view

--- Buildout76/BuildDetailView.swift
+++ Buildout76/BuildDetailView.swift
@@ -12,42 +12,86 @@ struct BuildDetailView: View {
     
     let columns = [GridItem(.adaptive(minimum: 150))]
     
+    enum FilterType: CaseIterable {
+        case strength, perception, endurance, charisma, intelligence, agility, luck
+    }
+    
     var body: some View {
         ScrollView {
-            LazyVGrid(columns: columns, spacing: 20) {
-                ForEach(build.perks.sorted { $0.perk.name < $1.perk.name }, id: \.id) { perk in
-                    ZStack(alignment: .bottomTrailing) {
-                        PerkCardSummaryView(perk: perk.perk)
-                            .frame(height: 230)
-                        
-                        VStack {
-                            if perk.perkLevel == 1 {
-                                Text(perk.perk.description1)
-                                    .padding()
-                            } else if perk.perkLevel == 2 {
-                                Text(perk.perk.description2 ?? "Unknown description.")
-                                    .padding()
-                            } else if perk.perkLevel == 3 {
-                                Text(perk.perk.description3 ?? "Unknown description.")
-                                    .padding()
-                            } else if perk.perkLevel == 4 {
-                                Text(perk.perk.description4 ?? "Unknown description.")
-                                    .padding()
-                            } else {
-                                Text(perk.perk.description5 ?? "Unknown description.")
-                                    .padding()
+                ForEach(FilterType.allCases, id: \.self) { filter in
+                LazyVGrid(columns: columns, spacing: 20) {
+                    ForEach(filteredBuildPerks(filter), id: \.id) { perk in
+                        ZStack(alignment: .bottomTrailing) {
+                            PerkCardSummaryView(perk: perk.perk)
+                                .frame(height: 230)
+                            
+                            VStack {
+                                if perk.perkLevel == 1 {
+                                    Text(perk.perk.description1)
+                                        .padding()
+                                } else if perk.perkLevel == 2 {
+                                    Text(perk.perk.description2 ?? "Unknown description.")
+                                        .padding()
+                                } else if perk.perkLevel == 3 {
+                                    Text(perk.perk.description3 ?? "Unknown description.")
+                                        .padding()
+                                } else if perk.perkLevel == 4 {
+                                    Text(perk.perk.description4 ?? "Unknown description.")
+                                        .padding()
+                                } else {
+                                    Text(perk.perk.description5 ?? "Unknown description.")
+                                        .padding()
+                                }
+                                
+                                
+                                Text("\(perk.perkLevel)/\(perk.perk.maxLevel)")
+                                    .padding(.bottom)
                             }
-                            
-                            
-                            Text("\(perk.perkLevel)/\(perk.perk.maxLevel)")
-                                .padding(.bottom)
                         }
                     }
                 }
+                .padding(.horizontal)
             }
-            .padding(.horizontal)
+            .navigationTitle(build.name)
         }
-        .navigationTitle(build.name)
+    }
+    
+    func title(_ filter: FilterType) -> String {
+        switch filter {
+        case .strength:
+            return "strength perks"
+        case .perception:
+            return "perception perks"
+        case .endurance:
+            return "endurance perks"
+        case .charisma:
+            return "charisma perks"
+        case .intelligence:
+            return "intelligence perks"
+        case .agility:
+            return "agility perks"
+        case .luck:
+            return "luck perks"
+        }
+    }
+    
+    func filteredBuildPerks(_ filter: FilterType) -> [PickedPerk] {
+        switch filter {
+        case .strength:
+            return build.perks.filter { $0.perk.specialCategory == "Strength" }
+        case .perception:
+            return build.perks.filter { $0.perk.specialCategory == "Perception" }
+        case .endurance:
+            return build.perks.filter { $0.perk.specialCategory == "Endurance" }
+        case .charisma:
+            return build.perks.filter { $0.perk.specialCategory == "Charisma" }
+        case .intelligence:
+            return build.perks.filter { $0.perk.specialCategory == "Intelligence" }
+        case .agility:
+            return build.perks.filter { $0.perk.specialCategory == "Agility" }
+        case .luck:
+            return build.perks.filter { $0.perk.specialCategory == "Luck" }
+        }
     }
 }
 
