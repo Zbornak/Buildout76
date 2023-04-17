8c86e5155902fc4bbd5872d2e8e1e06cae3943c5
Mark <zbornack@protonmail.com>
2023-04-17 16:25:00 +0100

    Added mutation struct

---
+++ Buildout76/Mutation.swift
@@ -0,0 +1,17 @@
+//
+//  Mutation.swift
+//  Buildout76
+//
+//  Created by Mark Strijdom on 17/04/2023.
+//
+
+import SwiftUI
+
+struct Mutation: Codable, Identifiable, Equatable {
+    var id = UUID()
+    let name: String
+    let descriptionPositive: String
+    let descriptionNegative: String
+    
+    static let example = Mutation(name: "Egg Head", descriptionPositive: "INT +6", descriptionNegative: "STR -3, END -3")
+}
