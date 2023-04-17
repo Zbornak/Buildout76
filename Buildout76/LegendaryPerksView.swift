981fb7ff12be17b1167b4fd0917dd1aa7cf559d9
Mark <zbornack@protonmail.com>
2023-04-17 10:52:58 +0100

    Added legendary perks and mutations views

---
+++ Buildout76/LegendaryPerksView.swift
@@ -0,0 +1,20 @@
+//
+//  LegendaryPerksView.swift
+//  Buildout76
+//
+//  Created by Mark Strijdom on 17/04/2023.
+//
+
+import SwiftUI
+
+struct LegendaryPerksView: View {
+    var body: some View {
+        Text("Choose your legendary perks.")
+    }
+}
+
+struct LegendaryPerksView_Previews: PreviewProvider {
+    static var previews: some View {
+        LegendaryPerksView()
+    }
+}
---
+++ Buildout76/MutationsView.swift
@@ -0,0 +1,20 @@
+//
+//  MutationsView.swift
+//  Buildout76
+//
+//  Created by Mark Strijdom on 17/04/2023.
+//
+
+import SwiftUI
+
+struct MutationsView: View {
+    var body: some View {
+        Text("Choose your mutations.")
+    }
+}
+
+struct MutationsView_Previews: PreviewProvider {
+    static var previews: some View {
+        MutationsView()
+    }
+}
