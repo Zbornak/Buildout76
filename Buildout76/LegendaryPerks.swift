cee192732b1d4f6187b46c4e312482c79db51b03
Mark <zbornack@protonmail.com>
2023-04-18 11:35:04 +0100

    Finished mutations and legendaryperks classes

--- Buildout76/LegendaryPerks.swift
+++ Buildout76/LegendaryPerks.swift
@@ -8,5 +8,41 @@
 import SwiftUI
 
 class LegendaryPerks: ObservableObject {
+    @Published var legendaryPerks: [LegendaryPerk]
     
+    private let saveKey = "LegendaryPerks"
+    
+    init() {
+        if let data = UserDefaults.standard.data(forKey: saveKey) {
+            if let decodedData = try? JSONDecoder().decode([LegendaryPerk].self, from: data) {
+                legendaryPerks = decodedData
+                return
+            }
+        }
+        
+        legendaryPerks = []
+    }
+    
+    func add(_ legendaryPerk: LegendaryPerk) {
+        objectWillChange.send()
+        legendaryPerks.append(legendaryPerk)
+        save()
+    }
+    
+    func save() {
+        if let encodedData = try? JSONEncoder().encode(legendaryPerks) {
+            UserDefaults.standard.set(encodedData, forKey: saveKey)
+        }
+    }
+    
+    func removePerk(_ legendaryPerk: LegendaryPerk) {
+        objectWillChange.send()
+        var indexes = IndexSet()
+        if let index = legendaryPerks.firstIndex(of: legendaryPerk) {
+            indexes.insert(index)
+        }
+        
+        legendaryPerks.remove(atOffsets: indexes)
+        save()
+    }
 }
--- Buildout76/Mutations.swift
+++ Buildout76/Mutations.swift
@@ -8,5 +8,41 @@
 import SwiftUI
 
 class Mutations: ObservableObject {
+    @Published var mutations: [Mutation]
     
+    private let saveKey = "Mutations"
+    
+    init() {
+        if let data = UserDefaults.standard.data(forKey: saveKey) {
+            if let decodedData = try? JSONDecoder().decode([Mutation].self, from: data) {
+                mutations = decodedData
+                return
+            }
+        }
+        
+        mutations = []
+    }
+    
+    func add(_ mutation: Mutation) {
+        objectWillChange.send()
+        mutations.append(mutation)
+        save()
+    }
+    
+    func save() {
+        if let encodedData = try? JSONEncoder().encode(mutations) {
+            UserDefaults.standard.set(encodedData, forKey: saveKey)
+        }
+    }
+    
+    func removePerk(_ mutation: Mutation) {
+        objectWillChange.send()
+        var indexes = IndexSet()
+        if let index = mutations.firstIndex(of: mutation) {
+            indexes.insert(index)
+        }
+        
+        mutations.remove(atOffsets: indexes)
+        save()
+    }
 }
