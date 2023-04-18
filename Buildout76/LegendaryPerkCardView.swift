224d1fd2404e0450cfb3a9d4e712da135706ee8f
Mark <zbornack@protonmail.com>
2023-04-18 12:52:47 +0100

    Created card views for mutations and legendary perks

---
+++ Buildout76/LegendaryPerkCardView.swift
@@ -0,0 +1,20 @@
+//
+//  LegendaryPerkCardView.swift
+//  Buildout76
+//
+//  Created by Mark Strijdom on 18/04/2023.
+//
+
+import SwiftUI
+
+struct LegendaryPerkCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LegendaryPerkCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        LegendaryPerkCardView()
+    }
+}
---
+++ Buildout76/MutationsCardView.swift
@@ -0,0 +1,20 @@
+//
+//  MutationsCardView.swift
+//  Buildout76
+//
+//  Created by Mark Strijdom on 18/04/2023.
+//
+
+import SwiftUI
+
+struct MutationsCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MutationsCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        MutationsCardView()
+    }
+}
