e2dfd7224eb2dac29325088d8c5714651fa5502e
Mark <zbornack@protonmail.com>
2023-04-19 09:09:34 +0100

    Made build example simpler instead of loading entire perks json again

--- Buildout76/Build.swift
+++ Buildout76/Build.swift
@@ -12,9 +12,6 @@ struct Build: Codable, Identifiable {
     let name: String
     var perks: [PickedPerk]
     
-    
-    
-    static let allPerks: [Perk] = Bundle.main.decode("perks.json")
-    static let pickedPerk = PickedPerk(perk: allPerks[0], perkLevel: 1)
+    static let pickedPerk = PickedPerk(perk: Perk(id: "super-dingus", name: "Super Dingus", specialCategory: "Strength", levelAvailable: 1, maxLevel: 3, description1: "Your dingus is now 10% better.", description2: "Your dingus is now 20% better.", description3: "Your dingus is now 30% better.", description4: "Your dingus is now 40% better.", description5: "Your dingus is now 50% better."), perkLevel: 1)
     static let example = Build(name: "Tha Killah", perks: [pickedPerk])
 }
