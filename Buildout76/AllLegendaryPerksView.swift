aea56c680bc5a2cf8f7b0ef462842c53bedf36d9
Mark <zbornack@protonmail.com>
2023-04-19 10:51:46 +0100

    Added all legendary perks to alllegendaryperksview

--- Buildout76/AllLegendaryPerksView.swift
+++ Buildout76/AllLegendaryPerksView.swift
@@ -8,13 +8,41 @@
 import SwiftUI
 
 struct AllLegendaryPerksView: View {
+    @EnvironmentObject var legendaryPerks: LegendaryPerks
+    @Environment(\.dismiss) var dismiss
+    
+    let allLegendaryPerks : [LegendaryPerk] = Bundle.main.decode("legendaryPerks.json")
+    
+    let columns = [GridItem(.adaptive(minimum: 150))]
+    
     var body: some View {
-        Text("Choose your legendary perks.")
+        NavigationView {
+            VStack {
+                ScrollView {
+                    LazyVGrid(columns: columns, spacing: 20) {
+                        ForEach(allLegendaryPerks, id: \.id) { legendaryPerk in
+                            LegendaryPerkCardView(legendaryPerk: legendaryPerk)
+                        }
+                    }
+                    .padding()
+                }
+            }
+            .toolbar {
+                Button {
+                    dismiss()
+                } label: {
+                    Image(systemName: "multiply")
+                        .fontWeight(.bold)
+                        .foregroundColor(.black)
+                }
+            }
+        }
     }
 }
 
 struct AllLegendaryPerksView_Previews: PreviewProvider {
     static var previews: some View {
         AllLegendaryPerksView()
+            .environmentObject(LegendaryPerks())
     }
 }
