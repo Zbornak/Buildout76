8188b281715a71c911c4d0dee222b5755d9b7333
Mark <zbornack@protonmail.com>
2023-04-21 16:43:59 +0100

    Updated equipment class

--- Buildout76/Equipment.swift
+++ Buildout76/Equipment.swift
@@ -11,16 +11,30 @@ class Equipment: ObservableObject {
     let rangedWeapons: [String]
     let underarmors: [String]
     let meleeWeapons: [String]
+    let armors: [String]
+    let powerArmors: [String]
+    
     let rangedPrefixes1: [String]
     let rangedPrefixes2: [String]
     let rangedPrefixes3: [String]
+    
     let meleePrefixes1: [String]
     let meleePrefixes2: [String]
     let meleePrefixes3: [String]
     
+    let armorPrefixes1: [String]
+    let armorPrefixes2: [String]
+    let armorPrefixes3: [String]
+    
+    let powerArmorPrefixes1: [String]
+    let powerArmorPrefixes2: [String]
+    let powerArmorPrefixes3: [String]
+    
     @Published var selectedRangedWeapon: String
     @Published var selectedUnderarmor: String
     @Published var selectedMeleeWeapon: String
+    @Published var selectedArmor: String
+    @Published var selectedPowerArmor: String
     
     @Published var selectedRangedPrefix1: String
     @Published var selectedRangedPrefix2: String
@@ -30,10 +44,20 @@ class Equipment: ObservableObject {
     @Published var selectedMeleePrefix2: String
     @Published var selectedMeleePrefix3: String
     
+    @Published var selectedArmorPrefix1: String
+    @Published var selectedArmorPrefix2: String
+    @Published var selectedArmorPrefix3: String
+    
+    @Published var selectedPowerArmorPrefix1: String
+    @Published var selectedPowerArmorPrefix2: String
+    @Published var selectedPowerArmorPrefix3: String
+    
     init() {
         rangedWeapons = ["Handmade Rifle", "The Fixer", "Gatling Gun"]
         underarmors = ["Secret Service", "Raider Leathers", "Vault76 Jumpsuit"]
         meleeWeapons = ["Bone Hammer", "Super Sledge", "Walking Cane"]
+        armors = []
+        powerArmors = []
         
         rangedPrefixes1 = ["Anti-Armor", "Bloodied", "Juggernauts"]
         rangedPrefixes2 = ["15% Faster Fire-Rate", "Explosive", "+50% V.A.T.S. Hit Chance"]
@@ -43,9 +67,19 @@ class Equipment: ObservableObject {
         meleePrefixes2 = ["Faster Swing Speed", "+25% Damage While Standing Still", "+40% Power Attack Damage"]
         meleePrefixes3 = ["90% Reduced Weight", "Breaks 50% slower", "+1 Strength"]
         
+        armorPrefixes1 = ["Anti-Armor", "Bloodied", "Juggernauts"]
+        armorPrefixes2 = ["25 Cryo Resistance", "Ammo Weight Reduction", "Weapon Weight Reduction"]
+        armorPrefixes3 = ["+1 Luck", "AP Cost Reduction", "50% More Durability"]
+        
+        powerArmorPrefixes1 = ["Anti-Armor", "Bloodied", "Juggernauts"]
+        powerArmorPrefixes2 = ["25 Cryo Resistance", "Ammo Weight Reduction", "Weapon Weight Reduction"]
+        powerArmorPrefixes3 = ["+1 Luck", "AP Cost Reduction", "50% More Durability"]
+        
         selectedRangedWeapon = "Handmade Rifle"
         selectedUnderarmor = "Secret Service"
         selectedMeleeWeapon = "Bone Hammer"
+        selectedArmor = "Forest Scout"
+        selectedPowerArmor = "T-65"
         
         selectedRangedPrefix1 = "Anti-Armor"
         selectedRangedPrefix2 = "15% Faster Fire-Rate"
@@ -54,5 +88,13 @@ class Equipment: ObservableObject {
         selectedMeleePrefix1 = "Bloodied"
         selectedMeleePrefix2 = "Faster Swing Speed"
         selectedMeleePrefix3 = "90% Reduced Weight"
+        
+        selectedArmorPrefix1 = "Unyielding"
+        selectedArmorPrefix2 = "Ammo Weight Reduction"
+        selectedArmorPrefix3 = "+1 Luck"
+        
+        selectedPowerArmorPrefix1 = "Juggernauts"
+        selectedPowerArmorPrefix2 = "Weapon Weight Reduction"
+        selectedPowerArmorPrefix3 = "AP Cost Reduction"
     }
 }
